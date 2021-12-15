2e76a237ec35e56afbd39706430fc1674bce44af
alber <evlakhov.oleg11@gmail.com>
2021-12-15 15:28:37 +0300

    Initial commit

--- app/src/main/java/com/example/c_05/MainActivity.kt
+++ app/src/main/java/com/example/c_05/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.c_05
 
 import android.os.Bundle
+
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.example.c_05.databinding.FragmentDiaryBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var toggle: ActionBarDrawerToggle
-
+    lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        drawerLayout = findViewById(R.id.drawerLayout)
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
@@ -22,11 +27,14 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         navView.setNavigationItemSelectedListener {
+
+            it.isChecked = true
+
             when (it.itemId) {
-                R.id.c_1 -> Toast.makeText(this, "Id cat_1", Toast.LENGTH_SHORT).show()
-                R.id.c_2 -> Toast.makeText(this, "Id cat_2", Toast.LENGTH_SHORT).show()
+                R.id.c_1 -> replaceFragment(ProfileFragment(), it.title.toString())
+                R.id.c_2 -> replaceFragment(DiaryFragment(), it.title.toString())
 //                R.id.c_2 -> startActivity(Intent(this, WeekViewActivity::class.java))
-                R.id.c_3 -> Toast.makeText(this, "Id cat_3", Toast.LENGTH_SHORT).show()
+                R.id.c_3 -> replaceFragment(DiaryFragment(), it.title.toString())
                 R.id.c_4 -> finish()
 
             }
@@ -34,6 +42,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun replaceFragment(fragment: Fragment, title: String){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
+    }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (toggle.onOptionsItemSelected(item)) {
             return true
