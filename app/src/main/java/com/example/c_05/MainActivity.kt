a9ff90e59c2b89450ac5c0cdb8644211578931c4
geloidd <evlakhov.oleg11@gmail.com>
2021-12-27 22:55:50 +0300

    Initial commit

--- app/src/main/java/com/example/c_05/MainActivity.kt
+++ app/src/main/java/com/example/c_05/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.c_05
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 
 import android.view.MenuItem
@@ -8,16 +9,39 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import com.example.c_05.databinding.FragmentDiaryBinding
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var toggle: ActionBarDrawerToggle
     lateinit var drawerLayout: DrawerLayout
+
+    lateinit var barList:ArrayList<BarEntry>
+
+    lateinit var barDataSet: BarDataSet
+    lateinit var barData: BarData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        barList= ArrayList()
+        barList.add(BarEntry(1f, 500f))
+        barList.add(BarEntry(2f, 100f))
+        barList.add(BarEntry(3f, 200f))
+        barList.add(BarEntry(4f, 300f))
+        barList.add(BarEntry(5f, 400f))
+        barList.add(BarEntry(6f, 600f))
+        barDataSet = BarDataSet(barList, "Progress")
+        barData=BarData(barDataSet)
+        barChart.data = barData
+        barDataSet.setColors(ColorTemplate.JOYFUL_COLORS, 250)
+        barDataSet.valueTextColor = Color.BLACK
+        barDataSet.valueTextSize = 15f
 
         drawerLayout = findViewById(R.id.drawerLayout)
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
@@ -50,6 +74,7 @@ class MainActivity : AppCompatActivity() {
         drawerLayout.closeDrawers()
         setTitle(title)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (toggle.onOptionsItemSelected(item)) {
             return true
