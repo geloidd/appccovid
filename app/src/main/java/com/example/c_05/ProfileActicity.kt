203207f2bf6268aad012d5c165793d76e15514bb
geloidd <evlakhov.oleg11@gmail.com>
2022-01-01 16:20:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/c_05/ProfileActicity.kt
@@ -0,0 +1,11 @@
+package com.example.c_05
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActicity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_acticity)
+    }
+}
